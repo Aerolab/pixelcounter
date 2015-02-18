cf8fef231a86a49b3e61027f0f6b5cb81a7552e7
Sergio Behrends <sergio@behrends.com.ar>
2015-02-18 16:09:55 -0300

    Refactor to make it more extensible

--- count.js
+++ count.js
@@ -1,102 +1,58 @@
 var fs = require('fs'),
-	stdio = require('stdio'),
 	sizeOf = require('image-size'),
 	path = require('path'),
 	walk = require('walk');
 
 
-var availableFormats = ['bmp', 'gif', 'jpg', 'jpeg', 'png', 'psd', 'tiff', 'webp', 'svg'],
-	scanFormats = [],
-	pixels = 0,
-	availableCommands = {
-    'dir': {key: 'd', description: 'Directory', args: 1},
-    'formats': {key: 'f', description: 'Image extensions separated with comma', args: 1}
-}
-for (i = 0; i < availableFormats.length; i++) {
-	availableCommands[availableFormats[i]] = {description: 'Count any ' + availableFormats[i] + ' file'};
+var availableFormats = function() {
+	return ['bmp', 'gif', 'jpg', 'jpeg', 'png', 'psd', 'tiff', 'webp', 'svg'];
 }
 
-var options = stdio.getopt(availableCommands);
+var startCounter = function(dir, scanFormats, onCount, onEnd) {
+	var pixels = 0;
+	if (scanFormats.length == 0) {
+		// If no format defined use all available
+		scanFormats = availableFormats();
+	}
+	if (!dir) {
+		// Set defautl folder in script path
+		dir = process.cwd();
+	}
 
-var directory = options.dir;
-if (!directory) {
-	// Set defautl folder in script path
-  directory = process.cwd();
-}
+	var walker  = walk.walk(dir, { followLinks: true });
 
-var formats = options.formats;
-if (formats) {
-	var inputFormats = formats.split(',');
-	for (i = 0; i < inputFormats.length; i++) {
-		if (availableFormats.indexOf(inputFormats[i]) > 0) {
-			scanFormats.push(inputFormats[i]);
+	walker.on('file', function(root, stat, next) {
+		var ext = path.extname(stat.name).split('.');
+		var ext = ext[ext.length - 1];
+
+		if (scanFormats.indexOf(ext.toLowerCase()) > 0) {
+
+			sizeOf(root + '/' + stat.name, function (err, dimensions) {
+				if (!err && typeof dimensions.width == "number" && typeof dimensions.height == "number") {
+					var filePixelsArea = dimensions.width*dimensions.height;
+					pixels += filePixelsArea;
+
+					// Partial counting
+					onCount(root + '/' + stat.name, filePixelsArea, pixels);
+
+					// Go next file
+					next();
+				} else {
+					next();
+				}
+			});
+			
+		} else {
+			next();
 		}
-	}
+	});
+
+	walker.on('end', function() {
+		onEnd(pixels);
+	});
 }
 
-for (i = 0; i < availableFormats.length; i++) {
-	if (options[availableFormats[i]]) {
-		if (scanFormats.indexOf(availableFormats[i]) == -1) {
-			scanFormats.push(availableFormats[i]);
-		}
-	}
-}
-
-
-if (scanFormats.length == 0) {
-	// If no format defined use all available
-	scanFormats = availableFormats;
-}
-
-printMsg('', true);
-printMsg("Starting to count pixels on:");
-printMsg(directory, true);
-printMsg('', true);
-printMsg("Image format to count: " + scanFormats.join(', '), true);
-printMsg('', true);
-
-var walker  = walk.walk(directory, { followLinks: true });
-
-printMsg("Counting pixels...", true);
-walker.on('file', function(root, stat, next) {
-    var ext = path.extname(stat.name).split('.');
-    var ext = ext[ext.length - 1];
-
-    if (scanFormats.indexOf(ext.toLowerCase()) > 0) {
-
-      sizeOf(root + '/' + stat.name, function (err, dimensions) {
-        if (!err && typeof dimensions.width == "number" && typeof dimensions.height == "number") {
-          var filePixelsArea = dimensions.width*dimensions.height;
-          pixels += filePixelsArea;
-          printMsg(pixels+" pixels and counting");
-          next();
-        } else {
-        	next();
-        }
-      });
-
-    } else {
-    	next();
-    }
-
-    
-});
-
-walker.on('end', function() {
-	if (pixels != 0) {
-	    printMsg("Total pixels counted: "+pixels);
-	    printMsg('', true);
-	} else {
-		printMsg("No valid images found :(");
-		printMsg('', true);
-	}
-});
-
-function printMsg(msg,enter) {
-	if (enter) {
-		process.stdout.write('\n');
-	}
-	process.stdout.clearLine();
-	process.stdout.cursorTo(0);
-	process.stdout.write(msg);
+module.exports = {
+	availableFormats: availableFormats,
+	start: startCounter
 }
