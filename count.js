6ff723b89576b1aad0dadbf81f2d3dfecba2810b
Israel Wegierski <israelmw@hotmail.com>
2015-02-26 17:32:11 -0300

    Reform logic to walk dirs
    The order of walk dirs afected before commit, then add variable to count dirs processed and executed callback end in last dir.

--- count.js
+++ count.js
@@ -5,64 +5,74 @@ var fs     = require('fs'),
     path   = require('path'),
     walk   = require('walk');
 
-
 var availableFormats = function() {
-	return ['bmp', 'gif', 'jpg', 'jpeg', 'png', 'psd', 'tiff', 'webp', 'svg'];
+  return ['bmp', 'gif', 'jpg', 'jpeg', 'png', 'psd', 'tiff', 'webp', 'svg'];
 };
 
 var startCounter = function(dir, scanFormats, onCount, onEnd) {
-	var pixels = 0;
+  var walker;
+  var pixels = 0;
+  var dirsProcessed = 0;
+
+  // If no format defined use all available
   if (scanFormats.length == 0) {
-		// If no format defined use all available
-		scanFormats = availableFormats();
-	}
-	if (!dir) {
-		// Set defautl folder in script path
-		dir = [process.cwd()];
-	}
-  if ( Array.isArray(dir) ) {
+    scanFormats = availableFormats();
+  }
 
-    var walker;
-    // For Array dir
-    dir.forEach(function (file) {
-      walker  = walk.walk(file, { followLinks: true });
-      // File
-      walker.on('file', function(root, stat, next) {
-        var ext = path.extname(stat.name).split('.');
-            ext = ext.pop();
+  // Set defautl folder in script path
+  if (!dir) {
+    dir = process.cwd();
+  }
+  // If dir is not array, create one
+  if ( !Array.isArray(dir) ) {
+    dir = [dir];
+  }
 
-        if (scanFormats.indexOf(ext.toLowerCase()) >= 0) {
-          sizeOf(root + '/' + stat.name, function (err, dimensions) {
-            if (!err && typeof dimensions.width == "number" && typeof dimensions.height == "number") {
-              var filePixelsArea = dimensions.width*dimensions.height;
-              pixels += filePixelsArea;
+  // For Array dir
+  dir.forEach(function (file) {
 
-              // Partial counting
-              onCount(root + '/' + stat.name, filePixelsArea, pixels);
+    walker  = walk.walk(file, { followLinks: true });
 
-              // Go next file
-              next();
-            } else {
-              next();
-            }
-          });
+    // File
+    walker.on('file', function(root, stat, next) {
+      var ext = path.extname(stat.name).split('.');
+          ext = ext.pop();
 
-        } else {
-          next();
-        }
+      if (scanFormats.indexOf(ext.toLowerCase()) >= 0) {
+        sizeOf(root + '/' + stat.name, function (err, dimensions) {
+          if (!err && typeof dimensions.width == "number" && typeof dimensions.height == "number") {
+            var filePixelsArea = dimensions.width*dimensions.height;
+            pixels += filePixelsArea;
 
-      });
+            // Partial counting
+            onCount(root + '/' + stat.name, filePixelsArea, pixels);
+
+            // Go next file
+            next();
+          } else {
+            next();
+          }
+        });
+
+      } else {
+        next();
+      }
+
     });
 
-  	walker.on('end', function() {
-  		onEnd(pixels);
-  	});
+    // On end
+    walker.on('end', function() {
+      dirsProcessed++;
+      if ( dirsProcessed >= dir.length ) {
+        onEnd(pixels);
+      }
+    });
 
-  }
+  });
 
 };
 
 module.exports = {
-	availableFormats: availableFormats,
+  availableFormats: availableFormats,
   start: startCounter
 };
